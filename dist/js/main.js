6812ce33798aaae27155a17a3ecdcfcc031d18d8
Jason Gange <jasongange@yahoo.com>
2020-08-13 23:56:25 +0800

    update

--- dist/js/main.js
+++ dist/js/main.js
@@ -6,6 +6,8 @@ const menuBranding =  document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 const body = document.querySelector('body')
 const spinner = document.querySelector('.lds-spinner')
+const download = document.querySelector('#download')
+
 
 
 
